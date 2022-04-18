30abb304a8d608a2f14bc8c96e7661282372178e
vdovinivan <99171081+vdovinivan@users.noreply.github.com>
2022-04-18 08:18:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,80 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 #include <iostream>
-#include <vector>
-using namespace std;
 
-void swap(int *a, int *b) {
-  int temp = *b;
-  *b = *a;
-  *a = temp;
+template<typename T, int size>
+class TPQueue {
+private:
+T arr[50];
+int write;
+int read;
+public:
+TPQueue() :write(0), read(0) { }
+void push(T x) {
+int cur = read++;
+while ((--cur >= write) && (arr[cur % size].prior < x.prior)) {
+arr[(cur + 1) % size] = arr[cur % size];
 }
-
-void heapify(vector<int> &hT, int i) {
-  int size = hT.size();
- 
-  int largest = i;
-  int l = 2 * i + 1;
-  int r = 2 * i + 2;
-  if (l < size && hT[l] > hT[largest])
-    largest = l;
-  if (r < size && hT[r] > hT[largest])
-    largest = r;
-
-  if (largest != i) {
-    swap(&hT[i], &hT[largest]);
-    heapify(hT, largest);
-  }
+arr[(cur + 1) % size] = x;
 }
-
-void insert(vector<int> &hT, int newNum) {
-  int size = hT.size();
-  if (size == 0) {
-    hT.push_back(newNum);
-  } else {
-    hT.push_back(newNum);
-    for (int i = size / 2 - 1; i >= 0; i--) {
-      heapify(hT, i);
-    }
-  }
+T pop() {
+return arr[(write++) % size];
 }
+};
 
-void deleteNode(vector<int> &hT, int num) {
-  int size = hT.size();
-  int i;
-  for (i = 0; i < size; i++) {
-    if (num == hT[i])
-      break;
-  }
-  swap(&hT[i], &hT[size - 1]);
-
-  hT.pop_back();
-  for (int i = size / 2 - 1; i >= 0; i--) {
-    heapify(hT, i);
-  }
-}
-
-void printArray(vector<int> &hT) {
-  for (int i = 0; i < hT.size(); ++i)
-    cout << hT[i] << " ";
-  cout << "\n";
-}
-
-int main() {
-  vector<int> heapTree;
-
-  insert(heapTree, 3);
-  insert(heapTree, 4);
-  insert(heapTree, 9);
-  insert(heapTree, 5);
-  insert(heapTree, 2);
-
-  cout << "Массив max-heap: ";
-  printArray(heapTree);
-
-  deleteNode(heapTree, 4);
-
-  cout << "После удаления элемента: ";
-
-  printArray(heapTree);
-}
+struct SYM {
+char ch;
+int prior;
